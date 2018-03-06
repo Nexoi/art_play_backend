4c4aca90ee1a23c7ce920490012aa88de70a4c0b
neo <nexoimy@gmail.com>
2018-03-07 02:45:05 +0800

    跨域问题解决

--- src/main/java/com/seeu/configurer/CorsFilter.java
+++ src/main/java/com/seeu/configurer/CorsFilter.java
@@ -3,8 +3,11 @@ package com.seeu.configurer;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 浏览器访问过滤器，解决跨域名请求等问题
@@ -12,9 +15,16 @@ import java.io.IOException;
 @Component
 public class CorsFilter implements Filter {
 
+    private List<String> allowedOrigins = Arrays.asList("http://localhost:8000");
+
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");//解决跨域请求问题
+        String origin = request.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "*");
+        response.setHeader("Vary", "Origin");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        // response.setHeader("Access-Control-Allow-Origin", "*");//解决跨域请求问题
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
