69fe72017c2f2768859103c4c4198f12ad8a0e2e
neo <nexoimy@gmail.com>
2018-03-09 06:49:47 +0800

    文件上传单独API

---
+++ src/main/java/com/seeu/apis/admin/files/FileUploadApi.java
@@ -0,0 +1,31 @@
+package com.seeu.apis.admin.files;
+
+import com.seeu.third.filestore.FileUploadService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Api(tags = "文件上传", description = "File")
+@RestController
+@RequestMapping("/api/admin/v1/upload")
+public class FileUploadApi {
+    @Autowired
+    private FileUploadService fileUploadService;
+
+    @PostMapping
+    public Map upload(MultipartFile file) throws IOException {
+        String url = fileUploadService.upload(file);
+        long size = file.getSize();
+        Map map = new HashMap();
+        map.put("url", url);
+        map.put("size", size);
+        return map;
+    }
+}
--- src/main/java/com/seeu/configurer/CorsFilter.java
+++ src/main/java/com/seeu/configurer/CorsFilter.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Component
 public class CorsFilter implements Filter {
 
-    private List<String> allowedOrigins = Arrays.asList("http://localhost:8000");
+    private List<String> allowedOrigins = Arrays.asList("http://localhost:8000", "*");
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
