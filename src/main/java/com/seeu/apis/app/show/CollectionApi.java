93433e74b2a91f857229cc1983041007d9116158
neo <nexoimy@gmail.com>
2018-05-15 10:33:53 +0800

    v1.1.2 update 2018-05-15 resources group fill webitems

--- src/main/java/com/seeu/apis/app/show/CollectionApi.java
+++ src/main/java/com/seeu/apis/app/show/CollectionApi.java
@@ -3,6 +3,7 @@ package com.seeu.apis.app.show;
 import com.seeu.artshow.collection.service.MyCollectionService;
 import com.seeu.artshow.exception.ResourceNotFoundException;
 import com.seeu.artshow.show.model.ResourceGroup;
+import com.seeu.artshow.show.service.ResourceGroupService;
 import com.seeu.artshow.userlogin.model.User;
 import com.seeu.core.R;
 import io.swagger.annotations.Api;
@@ -10,11 +11,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = "展览收藏", description = "查看个人收藏资源组")
 @RestController
@@ -23,6 +27,8 @@ public class CollectionApi {
 
     @Autowired
     private MyCollectionService myCollectionService;
+    @Autowired
+    private ResourceGroupService resourceGroupService;
 
     @ApiOperation("列出收藏【需登录用户】【分页】")
     @GetMapping
@@ -31,7 +37,13 @@ public class CollectionApi {
                                     @AuthenticationPrincipal User user,
                                     @RequestParam(defaultValue = "0") Integer page,
                                     @RequestParam(defaultValue = "10") Integer size) {
-        return myCollectionService.list(user.getUid(), new PageRequest(page, size, new Sort(Sort.Direction.DESC, "createTime")));
+        PageRequest pageRequest = new PageRequest(page, size, new Sort(Sort.Direction.DESC, "createTime"));
+        Page<ResourceGroup> groupPage = myCollectionService.list(user.getUid(), pageRequest);
+        return new PageImpl<ResourceGroup>(fillAllWebItemIds(groupPage.getContent()), pageRequest, groupPage.getTotalElements());
+    }
+
+    private List<ResourceGroup> fillAllWebItemIds(List<ResourceGroup> groups) {
+        return resourceGroupService.fillAllWebItemIdsByGroup(groups);
     }
 
     @ApiOperation("添加收藏【需登录用户】")
